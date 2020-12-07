101c2dd6a63d713f3209f6a55770c063b052ff39
Ella Dunn <75263435+21ebd2@users.noreply.github.com>
2020-12-07 10:23:49 -0500

    Add updated server and ui files

--- server.R
+++ server.R
@@ -8,19 +8,61 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(readr)
+library(plotly)
+library(tidyverse)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+mypath <- "C:/Users/Ella Dunn/Desktop/Data Visualization/Shiny/Final Project/Logistic-Regression/socialnetworkads.csv"
+data <- read_csv(mypath)
+data$Purchased <- factor(data$Purchased)
+gender <- data %>% select(Gender)
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output, session) {
+    
+    updateSelectInput(session, "gender", choices = c("Choose a Gender"="", gender))
+    
+    output$n1 <- renderValueBox({
+        count <- data %>% 
+            summarize(sum = sum(Purchased))
+        
+        valueBox(
+            value =  count,
+            subtitle = "Total Purchased",
+            icon = icon("table")
+        )
     })
-
+    
+    plotdata <- reactive({
+        if(input$rb == "age") {
+            data <- data %>% filter(xvariable == Age, Gender == input$gender)
+        }
+        else
+            if(input$rb == "salary") {
+                data <- data %>% filter(xvariable == EstimatedSalary, Gender == input$gender)
+            }
+        return(data)
+    })
+    
+    output$logisticplot <- renderPlotly({
+        
+        fit1 <- glm(plotdata()$Purchased ~ plotdata()$xvariable, family = binomial(link = logit))
+        
+        plottingdata <- tibble(
+            xvariable = xvariable.new,
+            log.odds = predict(fit1, newdata = data.frame(xvariable = xvariable.new), type = "link"),
+            odds = exp(log.odds),
+            prob = odds/(l+odds)
+        )
+        
+        p <- plot_ly(plotdata(),
+                     x = ~xvariable, y=~Purchased,
+                     type = 'scatter', mode = 'markers') %>%
+            layout(title = "Logistic Regression: Prob. of Purchase",
+                   yaxis = list("Probability of Purchase"))
+        return(p)
+        
+    })
+    
 })
